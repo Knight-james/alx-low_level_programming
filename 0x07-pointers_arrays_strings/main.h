6858215b35e9289407591605a1b66f1159dde42b
Knight-james <j.adegunna@yahoo.com>
2022-09-26 17:44:22 -0700

    More pointers

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+
+#endif /* MAIN_H */
